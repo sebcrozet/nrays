68006b25b697d40dbd15fddf3c4c9ca36874ea9b
Sébastien Crozet <developer@crozet.re>
2014-01-29 12:58:37 +0100

    Add perspective to the 4d demo.

--- examples/sphere4d.rs
+++ examples/sphere4d.rs
@@ -26,61 +26,64 @@ fn main() {
     let mut lights = ~[];
 
     {
-        lights.push(Light::new(Vec4::new(0.0f64, 10.0, 10.0, 1.0),
+        lights.push(Light::new(Vec4::new(10.0f64, -10.0, 10.0, 1.0),
                                0.0,
                                1,
-                               Vec3::new(1.0, 1.0, 1.0)));
-        lights.push(Light::new(Vec4::new(0.0f64, 10.0, 10.0, 1.0),
+                               Vec3::new(2.0, 2.0, 2.0)));
+        lights.push(Light::new(Vec4::new(-10.0f64, -10.0, 10.0, 1.0),
                                0.0,
                                1,
-                               Vec3::new(1.0, 1.0, 1.0)));
-        /*
-        lights.push(Light::new(Vec4::new(0.0f64, -10.0, -10.0, 1.0),
-                               Vec3::new(1.0, 1.0, 1.0)));
-        lights.push(Light::new(Vec4::new(0.0f64, -10.0, 10.0, 1.0),
-                               Vec3::new(1.0, 1.0, 1.0)));
-        lights.push(Light::new(Vec4::new(0.0f64, 10.0, -10.0, 1.0),
-                               Vec3::new(1.0, 1.0, 1.0)));
-        lights.push(Light::new(Vec4::new(0.0f64, 10.0, 10.0, 1.0),
-                               Vec3::new(1.0, 1.0, 1.0)));
-                               */
+                               Vec3::new(2.0, 2.0, 2.0)));
+        lights.push(Light::new(Vec4::new(10.0f64, 10.0, 10.0, 1.0),
+                               0.0,
+                               1,
+                               Vec3::new(2.0, 2.0, 2.0)));
+        lights.push(Light::new(Vec4::new(-10.0f64, 10.0, 10.0, 1.0),
+                               0.0,
+                               1,
+                               Vec3::new(2.0, 2.0, 2.0)));
+        // lights.push(Light::new(Vec4::new(0.0f64, 10.0, 10.0, 1.0),
+        //                        0.0,
+        //                        1,
+        //                        Vec3::new(1.0, 1.0, 1.0)));
     }
 
     let blue = Arc::new(~PhongMaterial::new(
-        Vec3::new(0.1, 0.1, 0.1),
+        Vec3::new(1.0, 1.0, 1.0),
         Vec3::new(1.0, 1.0, 1.0),
         Vec3::new(1.0, 1.0, 1.0),
         None,
         None,
-        100.0
+        60.0
     ) as ~Material:Freeze+Send);
 
     let transform: Iso4<f64> = na::one();
 
-    let box_shape = ~Box::new_with_margin(Vec4::new(0.25, 0.25, 0.25, 0.00000001), 0.000000001);
+    let box_shape = ~Box::new_with_margin(Vec4::new(0.25, 0.25, 0.25, 0.25), 0.0);
     let ball      = ~Ball::new(0.25);
     let cone      = ~Cone::new_with_margin(0.25, 0.25, 0.0);
     let cylinder  = ~Cylinder::new_with_margin(0.25, 0.25, 0.0);
 
-    let pos  = na::append_translation(&transform, &Vec4::new(0.0, 0.0, 0.0,    4.0));
-    let pos2 = na::append_translation(&transform, &Vec4::new(0.75, 0.75, 0.0,  4.0));
-    let pos3 = na::append_translation(&transform, &Vec4::new(0.0, 0.75, 0.75,  4.0));
-    let pos4 = na::append_translation(&transform, &Vec4::new(0.0, 0.75, -0.75, 4.0));
+    let pos  = na::append_translation(&transform, &Vec4::new(0.0, 0.0, 0.0,    1.5));
+    let pos2 = na::append_translation(&transform, &Vec4::new(0.75, 0.75, 0.0,  1.5));
+    let pos3 = na::append_translation(&transform, &Vec4::new(0.0, 0.75, 0.75,  1.5));
+    let pos4 = na::append_translation(&transform, &Vec4::new(0.0, 0.75, -0.75, 1.5));
 
     let mut nodes = ~[];
-    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.4, 0.2, 1.0, 1.0, pos,  ball, None, true)));
-    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.4, 0.2, 1.0, 1.0, pos2, box_shape, None, true)));
-    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.4, 0.2, 1.0, 1.0, pos3, cone, None, true)));
-    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.4, 0.2, 1.0, 1.0, pos4, cylinder, None, true)));
+    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.0, 0.0, 1.0, 1.0, pos,  ball, None, true)));
+    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.0, 0.0, 1.0, 1.0, pos2, box_shape, None, true)));
+    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.0, 0.0, 1.0, 1.0, pos3, cone, None, true)));
+    nodes.push(Arc::new(SceneNode::new(blue.clone(), 0.0, 0.0, 1.0, 1.0, pos4, cylinder, None, true)));
 
     let scene = Scene::new(nodes, lights);
     let pixels = scene.render(&resolution, |pt| {
         let x = (pt.x / resolution.x - 0.5) * 2.0;
         let y = (pt.y / resolution.y - 0.5) * 2.0;
         let z = (pt.z / resolution.z - 0.5) * 2.0;
-        // Ray::new(eye, na::normalize(&(Vec4::new(pt.x, pt.y, pt.z, 1.0) - eye)))
+        let eye = Vec4::new(0.0, 0.0, 0.0, 0.0);
+        Ray::new(eye, na::normalize(&(Vec4::new(x, y, z, 1.0) - eye)))
 
-        Ray::new(Vec4::new(x, y, z, 0.0), Vec4::new(0.0, 0.0, 0.0, 1.0))
+        // Ray::new(Vec4::new(x, y, z, 0.0), Vec4::new(0.0, 0.0, 0.0, 1.0))
     });
 
     let path = "out.4d";
